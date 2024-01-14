b9b6f448d5b0c6ac98431a6ca571cab81fea3987
Toni Koskinen <103842586+ToniKoskinen@users.noreply.github.com>
2024-01-14 14:22:23 +0200

    Delete manytomanyexample.sql
    Deleted an unnecessary file

--- manytomanyexample.sql
+++
@@ -1,5 +0,0 @@
-SELECT name, seasonid FROM PlayersInSeason
-INNER JOIN Player ON PlayersInSeason.FK_playerid=Player.playerid
-INNER JOIN Season ON PlayersInSeason.FK_seasonid=Season.seasonid
-ORDER BY name
-LIMIT 15
